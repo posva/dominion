72df90674c14a37cd6cbacba83b7d3b91e76cbe9
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-09-14 18:16:26 +0200

    up

---
+++ src/treasure.js
@@ -0,0 +1,11 @@
+define(["lib/selfish"], function(selfish) {
+    var Base = selfish.Base;
+    var Treasure = Base.extend({
+        initialize: function(money) {
+            this.money = money;
+            this.type.push('treasure');
+        },
+    });
+
+    return Treasure;
+});
