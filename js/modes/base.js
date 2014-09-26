fad727972d501b964cde88e3c38fcc3671bcb05a
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-09-26 23:24:08 +0200

    - lodash - test for modes (should evolve) - base mode WIP

--- js/modes/base.js
+++ js/modes/base.js
@@ -1,4 +1,24 @@
-define(['cards/copper', 'cards/curse', 'cards/duchy', 'cards/estate', 'cards/gold', 'cards/province', 'cards/silver'], function(Copper, Curse, Duchy, Estate, Gold, Province, Silver) {
+define(['lodash', 'cards/copper', 'cards/curse', 'cards/duchy', 'cards/estate', 'cards/gold', 'cards/province', 'cards/silver'], function(_, Copper, Curse, Duchy, Estate, Gold, Province, Silver) {
+  var amounts = {
+    '2': { // TODO
+      'curse': 10,
+      'estate': 8,
+      'copper': 40,
+      'province': 8,
+      'duchy': 8,
+      'silver': 30,
+      'gold': 30
+    },
+    '3-4': { // TODO
+      'curse': 30,
+      'estate': 12,
+      'copper': 40,
+      'province': 12,
+      'duchy': 12,
+      'silver': 30,
+      'gold': 30
+    }
+  };
   var Base = {
     cards: {
       'curse': Curse,
@@ -9,25 +29,28 @@ define(['cards/copper', 'cards/curse', 'cards/duchy', 'cards/estate', 'cards/gol
       'silver': Silver,
       'gold': Gold
     },
-    amounts : {
-      '2': { // TODO
-        'curse': 10,
-        'estate': 8,
-        'copper': 40,
-        'province': 8,
-        'duchy': 8,
-        'silver': 30,
-        'gold': 30
-      },
-      '3-4': { // TODO
-        'curse': 30,
-        'estate': 12,
-        'copper': 40,
-        'province': 12,
-        'duchy': 12,
-        'silver': 30,
-        'gold': 30
-      }
+    getAmounts: function(n) {
+      var r;
+      n = n || 0;
+      if (typeof n !== 'number')
+        return r; // only numbers please
+      _.forOwn(amounts, function(v, k) {
+        var ind = k.indexOf('-'), a, b;
+        if (ind > 0) {
+          a = parseInt(k.substr(0, ind));
+          b = parseInt(k.substr(ind+1), 10);
+          if (n >= a && n <= b) {
+            r = v;
+            return false;
+          }
+        } else {
+          if (n === parseInt(k, 10)) {
+            r = v;
+            return false;
+          }
+        }
+      });
+      return r;
     }
   };
 
---
+++ test/modes.js
@@ -0,0 +1,72 @@
+var requirejs = require('requirejs');
+var assert = require('assert');
+var should = require('should');
+var _ = require('lodash');
+var fs = require('fs');
+requirejs.config({
+  baseUrl: 'js',
+  nodeRequire: require
+});
+
+describe('Dummy', function() {
+  var modes = {}, modes_array = [];
+  before(function(done) {
+    fs.readdir('js/modes', function(err, files) {
+      // find modes files
+      _.forEach(files, function(v) {
+        if (v.match(/.js$/)) {
+          var new_mode = 'modes/' + v.replace(/.js$/, '');
+          modes_array.push(new_mode);
+          modes[new_mode] = null;
+        }
+      });
+      // require them
+      requirejs(modes_array, function() {
+        var i;
+        for (i = 0; i < modes_array.length; i++) {
+          modes[modes_array[i]] = arguments[i];
+        }
+        describe('Modes testing', function() {
+          _.forOwn(modes, function(v, k) {
+            describe('#' + k+ '.js', function(){
+              v = modes[k];
+              it('should have a cards object', function(){
+                v.should.have.property('cards');
+              });
+              it('should be able to get amounts of cards', function(){
+                v.getAmounts.should.be.a.Function;
+                assert.strictEqual(v.getAmounts('1'), undefined);
+                assert.strictEqual(v.getAmounts({}), undefined);
+                assert.strictEqual(v.getAmounts([]), undefined);
+                assert.strictEqual(v.getAmounts(1), undefined);
+                v.getAmounts(2).should.be.ok.and.be.an.Object;
+                v.getAmounts(3).should.be.ok.and.be.an.Object;
+                v.getAmounts(4).should.be.ok.and.be.an.Object;
+              });
+              it('should have an amount for every card in \'cards\'', function() {
+                var i, amounts;
+                for (i = 2; i < 6; i++) {
+                  amounts = v.getAmounts(i);
+                  if (amounts) {
+                    _.forOwn(v.cards, function(v, k) {
+                      amounts.should.have.property(k).and.be.a.Number;
+                    });
+                  }
+                }
+              });
+            });
+          });
+        });
+        done();
+      });
+    });
+  });
+
+    it(':D', function(done) {
+      var a = {};
+      a.should.be.ok;
+      done();
+    });
+
+  // TODO automate the test for every mode
+});
