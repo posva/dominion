874a1afdda1cb69b4eefe1ce87c204056eb4cdca
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-09-29 12:26:15 +0200

    script now generate a valid js file

--- scripts/gen-cards-deps.py
+++ scripts/gen-cards-deps.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python3
 #
 # Generate requirejs deps from a set of cards.
-# Place yourself in the directory where are the cards. For example in js/cards
-# where the base cards (copper, curse) are. Then launch the script:
-# ../../script/genCardsDeps.py *.js
-# -> define(['cards/copper', 'cards/curse', 'cards/duchy', 'cards/estate', 'cards/gold', 'cards/province', 'cards/silver'], function(Copper, Curse, Duchy, Estate, Gold, Province, Silver) {
-# {'curse': Curse, 'estate': Estate, 'copper': Copper, 'province': Province, 'duchy': Duchy, 'silver': Silver, 'gold': Gold}
+# ./gen-cards-deps.py ../js/cards/*.js
+# /gen-cards-deps.py ../js/cards/* | js-beautify -s 2 -f -
+# /gen-cards-deps.py ../js/cards/* | js-beautify -s 2 -f - > file.js
+#define(['cards/copper', 'cards/curse', 'cards/duchy', 'cards/estate', 'cards/gold', 'cards/province', 'cards/silver'], function(Copper, CurseCard, Duchy, Estate, Gold, Province, Silver) {
+#  return {
+#    'silver': Silver,
+#    'estate': Estate,
+#    'curse': CurseCard,
+#    'province': Province,
+#    'duchy': Duchy,
+#    'gold': Gold,
+#    'copper': Copper
+#  };
+#});
 
 import getopt, sys, time, os, re
 
@@ -26,13 +35,16 @@ names = [] # function(Copper, ...) {
 nar = re.compile('js/(.*)\.js')
 obj = {} # dic name -> card
 for f in sys.argv[1:]:
-    ckey = nar.search(os.path.abspath(f)).group(1)
+    try:
+        ckey = nar.search(os.path.abspath(f)).group(1)
+    except:
+        print(f+' doesn\'t seem to be a card. It will be ignored', file=sys.stderr)
+        continue
     cval = getReturn(f)
 
     names.append(cval)
     dep.append(ckey)
     obj[os.path.basename(ckey)] = cval
 
-print('define(%s, function(%s) {'%(str(dep), str(names).replace('[',
-'').replace(']', '').replace("'", '')))
-print(re.sub(r'\'([a-zA-Z_]+)\'([,}])', r'\1\2', str(obj)))
+print('define(%s, function(%s) {'%(str(dep), str(names).replace('[', '').replace(']', '').replace("'", '')))
+print('return', re.sub(r'\'([a-zA-Z_]+)\'([,}])', r'\1\2', str(obj)), '; });')
