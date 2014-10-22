2de7c5fba72660e4bb8385f2c98ef19a0b668534
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-10-22 15:18:52 +0200

    updated mode test description

--- test/modes.js
+++ test/modes.js
@@ -9,7 +9,7 @@ requirejs.config({
   nodeRequire: require
 });
 
-describe('Dummy', function() {
+describe('Dummy Suite for Modes', function() {
   var modes = {}, modes_array = [], Player;
   before(function(done) {
     requirejs(['player'], function(player) {
@@ -31,38 +31,40 @@ describe('Dummy', function() {
           }
           describe('Modes testing', function() {
             _.forOwn(modes, function(v, k) {
-              describe('#' + k+ '.js', function(){
+              describe(k+ '.js', function(){
                 v = modes[k];
-                it('should have a cards object', function(){
-                  v.should.have.property('cards');
-                });
-                it('should be able to get amounts of cards', function(){
-                  v.getAmounts.should.be.a.Function;
-                  assert.strictEqual(v.getAmounts('1'), undefined);
-                  assert.strictEqual(v.getAmounts({}), undefined);
-                  assert.strictEqual(v.getAmounts([]), undefined);
-                  assert.strictEqual(v.getAmounts(1), undefined);
-                  v.getAmounts(2).should.be.ok.and.be.an.Object;
-                  v.getAmounts(3).should.be.ok.and.be.an.Object;
-                  v.getAmounts(4).should.be.ok.and.be.an.Object;
-                });
-                it('should have an amount for every card in \'cards\'', function() {
-                  var i, amounts;
-                  var floop = function(v, k) {
-                    amounts.should.have.property(k).and.be.a.Number;
-                  };
-                  for (i = 2; i < 6; i++) {
-                    amounts = v.getAmounts(i);
-                    if (amounts) {
-                      _.forOwn(v.cards, floop);
+                describe('#Common', function(){
+                  it('should have a cards object', function(){
+                    v.should.have.property('cards');
+                  });
+                  it('should be able to get amounts of cards', function(){
+                    v.getAmounts.should.be.a.Function;
+                    assert.strictEqual(v.getAmounts('1'), undefined);
+                    assert.strictEqual(v.getAmounts({}), undefined);
+                    assert.strictEqual(v.getAmounts([]), undefined);
+                    assert.strictEqual(v.getAmounts(1), undefined);
+                    v.getAmounts(2).should.be.ok.and.be.an.Object;
+                    v.getAmounts(3).should.be.ok.and.be.an.Object;
+                    v.getAmounts(4).should.be.ok.and.be.an.Object;
+                  });
+                  it('should have an amount for every card in \'cards\'', function() {
+                    var i, amounts;
+                    var floop = function(v, k) {
+                      amounts.should.have.property(k).and.be.a.Number;
+                    };
+                    for (i = 2; i < 6; i++) {
+                      amounts = v.getAmounts(i);
+                      if (amounts) {
+                        _.forOwn(v.cards, floop);
+                      }
                     }
-                  }
+                  });
+                  it('should have a working playerInitializer', function() {
+                    v.playerInitializer.should.be.a.Function;
+                    Player.new.bind(Player, v.playerInitializer).should.not.throw();
+                  });
                 });
-                it('should have a working playerInitializer', function() {
-                  v.playerInitializer.should.be.a.Function;
-                  Player.new.bind(Player, v.playerInitializer).should.not.throw();
-                });
-                describe('Card creation', function() {
+                describe('#Card creation', function() {
                   _.forOwn(v.cards, function(c, name) {
                     it('should be able to create ' + name, function() {
                       c.new().should.be.ok;
