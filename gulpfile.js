07c290d931fdba1e0d377e5432061f5e69cc2a3b
Eduardo San Martin Morote <i@posva.net>
2015-07-26 11:51:32 +0200

    Fixed gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task('lint:coffee', function() {
 gulp.task('lint', ['lint:js', 'lint:coffee']);
 
 gulp.task('test', function(done) {
-  return gulp.src(['js/**/*.js', '@(server|client)/**/*.coffee'])
+  gulp.src(['js/**/*.js', '@(server|client)/**/*.coffee'])
     .pipe(istanbul()) // Covering files
     .pipe(istanbul.hookRequire()) // Force `require` to return covered files
     .on('finish', function() {
